ac739d62f7daf499069c297609fb7e84c50d6772
gaoyiha1114 <2797345@qq.com>
2026-01-08 09:05:38 +0800

    Update hy2.sh

--- hy2.sh
+++ hy2.sh
@@ -7,7 +7,7 @@ set -e
 
 # ---------- 默认配置 ----------
 HYSTERIA_VERSION="v2.6.5"
-const DOMAIN = await getVariableValue('DOMAIN', 'jp.gaoyigao.qzz.io')
+const DOMAIN=await getVariableValue('DOMAIN', 'jp.gaoyigao.qzz.io')
 DEFAULT_PORT=9527         # 自适应端口
 AUTH_PASSWORD="ieshare2025"   # 建议修改为复杂密码
 CERT_FILE="cert.pem"
@@ -142,6 +142,7 @@ main() {
 }
 
 main "$@"
+
 
 
 
